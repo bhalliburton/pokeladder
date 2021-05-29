bfcc4d507edfab1cc2da46489419259c0a9f62df
Brent Halliburton <bhalliburton@gmail.com>
2021-05-29 16:10:36 -0400

    Added Google Analytics

--- resources/views/themes/tailwind/layouts/app.blade.php
+++ resources/views/themes/tailwind/layouts/app.blade.php
@@ -45,6 +45,16 @@
     <!-- Styles -->
     <link href="{{ asset('themes/' . $theme->folder . '/css/app.css') }}" rel="stylesheet">
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C374ZYNDJJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C374ZYNDJJ');
+</script>
+
     @livewireStyles
 </head>
 <body class="flex flex-col min-h-screen @if(Request::is('/')){{ 'bg-white' }}@else{{ 'bg-gray-50' }}@endif @if(config('wave.dev_bar')){{ 'pb-10' }}@endif">
